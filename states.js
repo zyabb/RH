fbe3bb56174cf80800ef9e392b9488a82d9512df
Ruslan Zyabbarov <48525052+zyabb@users.noreply.github.com>
2022-07-27 14:32:15 -0400

    Create states.js

---
+++ states.js
@@ -0,0 +1,434 @@
+const states = {
+  Alabama: {
+    MSA: {
+      Huntsville: {
+        "Limestone County": {},
+        "Madison County": {},
+      },
+      "Birmingham-Hover": {
+        "Bibb County": {},
+        "Blount County": {},
+        "Chilton County": {},
+        "Jefferson County": {},
+        "St. Clair County": {},
+        "Shelby County": {},
+      },
+    },
+  },
+  Arkansas: {
+    MSA: {
+      "Little Rock-North Little Rock-Conway": {
+        "Faulkner County": {},
+        "Grant County": {},
+        "Lonoke County": {},
+        "Perry County": {},
+        "Pulaski County": {},
+        "Saline County": {},
+      },
+    },
+  },
+  Colorado: {
+    MSA: {
+      "Denver-Aurora-Lakewood": {
+        "Adams County": {},
+        "Arapahoe County": {},
+        "Broomfield County": {},
+        "Clear Creek County": {},
+        "Denver County": {},
+        "Douglas County": {},
+        "Elbert County": {},
+        "Gilpin County": {},
+        "Jefferson County": {},
+        "Park County": {},
+      },
+      "Fort Collins-Loveland": {
+        "Larimer County": {},
+      },
+      "Colorado Springs": {
+        "El Paso County": {},
+        "Teller County": {},
+      },
+    },
+  },
+  Florida: {
+    MSA: {
+      "Tampa-St. Petersburg-Clearwater": {
+        "Hernanondo County": {},
+        "Pasco County": {},
+        "Pinellas County": {},
+        "Hillsborough County": {},
+      },
+      "Deltona-Daytona Beach-Ormond Beach": {
+        "Flagler County": {},
+        "Volusia County": {},
+      },
+      Gainesville: {
+        "Alachua County": {},
+        "Gilchrist County": {},
+        "Levy County": {},
+      },
+      Jacksonville: {
+        "Baker County": {},
+        "Clay County": {},
+        "Duval County": {},
+        "Nassau County": {},
+        "St. Johns County": {},
+      },
+      "Lakeland-Winter Haven": {
+        "Polk County": {},
+      },
+      "North Port-Sarasota-Bradenton": {
+        "Manatee County": {},
+        "Sarasota County": {},
+      },
+      Ocala: {
+        "Marion County": {},
+      },
+      "Orlando-Kissimmee-Sanford": {
+        "Lake County": {},
+        "Orange County": {},
+        "Osceola County": {},
+        "Seminole County": {},
+      },
+    },
+  },
+  Georgia: {
+    MSA: {
+      "Atlanta-Sandy Springs-Alpharetta": {
+        "Spalding County": {},
+        "Barrow County": {},
+        "Bartow County": {},
+        "Butts County": {},
+        "Carroll County": {},
+        "Cherokee County": {},
+        "Clayton County": {},
+        "Cobb County": {},
+        "Coweta County": {},
+        "Dawson County": {},
+        "DeKalb County": {},
+        "Douglas County": {},
+        "Fayette County": {},
+        "Forsyth County": {},
+        "Fulton County": {},
+        "Gwinnett County": {},
+        "Haralson County": {},
+        "Herald County": {},
+        "Henry County": {},
+        "Jasper County": {},
+        "Lamar County": {},
+        "Meriwether County": {},
+        "Morgan County": {},
+        "Newton County": {},
+        "Paulding County": {},
+        "Pickens County": {},
+        "Pike County": {},
+        "Rockdale County": {},
+        "Walton County": {},
+      },
+      Gainesville: {
+        "Hall County": {},
+      },
+    },
+  },
+  Indiana: {
+    MSA: {
+      "Indianapolis-Carmel-Anderson": {
+        "Boone County": {},
+        "Brown County": {},
+        "Hamilton County": {},
+        "Hancock County": {},
+        "Hendricks County": {},
+        "Johnson County": {},
+        "Madison County ": {},
+        "Marion County": {},
+        "Morgan County": {},
+        "Putnam County": {},
+        "Shelby County": {},
+      },
+    },
+    Kentucky: {
+      MSA: {
+        "Louisville-Jefferson": {
+          "Bullitt County": {},
+          "Henry County": {},
+          "Jefferson County": {},
+          "Oldham County": {},
+          "Shelby County": {},
+          "Spencer County": {},
+        },
+      },
+    },
+    Missouri: {
+      MSA: {
+        "St. Louis": {
+          "Franklin County": {},
+          "Jefferson County": {},
+          "Lincoln County": {},
+          "St. Charles County": {},
+          "St. Louis County": {},
+          "Warren County": {},
+        },
+      },
+    },
+    "North Carolina": {
+      MSA: {
+        Asheville: {},
+        Burlington: {},
+        "Charlotte-Concord-Gastonia": {},
+        "Durham-Chapel Hill": {},
+        Fayetteville: {},
+        Goldsboro: {},
+        "Greensboro-High Point": {},
+        Greenville: {},
+        "Hickory-Lenoir-Morganton": {},
+        Jacksonville: {},
+        "Myrtle Beach-Conway-North Myrtle Beach": {},
+        "New Bern": {},
+        "Raleigh-Cary": {},
+        "Rocky Mount": {},
+        "Virginia Beach-Norfolk-Newport News": {},
+        Wilmington: {},
+        "Winston-Salem": {},
+        "Charleston-North Charleston": {},
+      },
+    },
+    "North Carolina": {
+      MSA: {
+        Asheville: {
+          "Buncombe County": {},
+          "Haywood County": {},
+          "Henderson County": {},
+          "Madison County": {},
+        },
+        Burlington: {
+          "Alamance County": {},
+        },
+        "Charlotte-Concord-Gastonia": {
+          "Anson County": {},
+          "Cabarrus County": {},
+          "Gaston County": {},
+          "Iredell County": {},
+          "Lancaster County": {},
+          "Lincoln County": {},
+          "Mecklenburg County": {},
+          "Rowan County": {},
+          "Union County": {},
+          "Stanly County": {},
+          "Cleveland County": {},
+        },
+        "Durham-Chapel Hill": {
+          "Chatham County": {},
+          "Durham County": {},
+          "Granville County": {},
+          "Orange County": {},
+          "Person County": {},
+        },
+        Fayetteville: {
+          "Cumberland County": {},
+          "Harnett County": {},
+          "Hoke County": {},
+        },
+        Goldsboro: {
+          "Wayne County": {},
+        },
+        "Greensboro-High Point": {
+          "Guliford County": {},
+          "Rockingham County": {},
+          "Randolph County": {},
+        },
+        Greenville: {
+          "Pitt County": {},
+        },
+        "Hickory-Lenoir-Morganton": {
+          "Alexander County": {},
+          "Burke County": {},
+          "Caldwell County": {},
+          "Catawba County": {},
+        },
+        Jacksonville: { "Onslow County": {} },
+        "Myrtle Beach-Conway-North Myrtle Beach": {
+          "Brunswick County": {},
+        },
+        "New Bern": {
+          "Craven County": {},
+          "Jones County": {},
+          "Pamlico County": {},
+        },
+        "Raleigh-Cary": {
+          "Franklin County": {},
+          "Johnston County": {},
+          "Wake County": {},
+        },
+        "Rocky Mount": {
+          "Edgecombe County": {},
+          "Nash County": {},
+        },
+        "Virginia Beach-Norfolk-Newport News": {
+          "Camden County": {},
+          "Currituk County": {},
+          "Gates County": {},
+        },
+        Wilmington: {
+          "New Hanover County": {},
+          "Pender County": {},
+        },
+        "Winston-Salem": {
+          "Davidson County": {},
+          "Davie County": {},
+          "Forsyth County": {},
+          "Stokes County": {},
+          "Yadkin County": {},
+        },
+        "Charleston-North Charleston": {
+          "Berkeley County": {},
+          "Charleston County": {},
+          "Dorchester County": {},
+        },
+      },
+    },
+    Ohio: {
+      MSA: {
+        Cincinnati: {
+          "Brown County": {},
+          "Butler County": {},
+          "Clermont County": {},
+          "Hamilton County": {},
+          "Warren County": {},
+          Clinton: {},
+        },
+        "Dayton-Kettering": {
+          "Greene County": {},
+          "Miami County": {},
+          "Montgomery County": {},
+        },
+        Columbus: {
+          "Delaware County": {},
+          "Fairfield County": {},
+          "Franklin County": {},
+          "Hocking County": {},
+          "Licking County": {},
+          "Madison County": {},
+          "Morrow County": {},
+          "Perry County": {},
+          "Pickaway County": {},
+          "Union County": {},
+        },
+      },
+    },
+    Oklahoma: {
+      MSA: {
+        "Oklahoma City": {
+          "Canadian County": {},
+          "Cleveland County": {},
+          "Grady County": {},
+          "Lincoln County": {},
+          "Logan County": {},
+          "McClain County": {},
+          "Oklahoma County": {},
+        },
+      },
+    },
+    "South Carolina": {
+      MSA: {
+        "Charlotte-Concord-Gastonia": {
+          "Chester County": {},
+          "Lancaster County": {},
+          "York County": {},
+        },
+        Columbia: {
+          "Calhoun County": {},
+          "Fairfield County": {},
+          "Kershaw County": {},
+          "Lexington County": {},
+          "Richland County": {},
+          "Saluda County": {},
+        },
+        Florence: {
+          "Darlington County": {},
+          "Florence County": {},
+        },
+        "Greenville-Anderson": {
+          "Andreson County": {},
+          "Greenville County": {},
+          "Laurens County": {},
+          "Pickens County": {},
+        },
+        "Hilton Head Island-Bluffton": {
+          "Beaufort County": {},
+          "Jasper County": {},
+        },
+        "Myrtle Beach-Conway-North Myrtle Beach": {
+          "Horry County": {},
+        },
+        Spartanburg: {
+          "Spartanburg County": {},
+        },
+        Sumter: {
+          "Clarendon County": {},
+          "Sumter County": {},
+        },
+      },
+    },
+    Tennessee: {
+      MSA: {
+        Chattanooga: {
+          "Hamilton County": {},
+          "Marion County": {},
+          "Sequatchie County": {},
+        },
+        Clarksville: {
+          "Montgomery County": {},
+          "Stewart County": {},
+        },
+        Cleveland: {
+          "Bradley County": {},
+          "Polk County": {},
+        },
+        "Johnson City": {
+          "Carter County": {},
+          "Unicoi County": {},
+          "Washington County": {},
+        },
+        "Kingsport-Bristol": {
+          "Hawkins County": {},
+          "Sullivan County": {},
+        },
+        Knoxville: {
+          "Anderson County": {},
+          "Blount County": {},
+          "Campbell County": {},
+          "Knox County": {},
+          "Loudon County": {},
+          "Morgan County": {},
+          "Roane County": {},
+          "Union County": {},
+        },
+        Memphis: {
+          "Fayette County": {},
+          "Shelby County": {},
+          "Tipton County": {},
+        },
+        Morristown: {
+          "Grainger County": {},
+          "Hambien County": {},
+          "Jefferson County": {},
+        },
+        "Nashville-Davidson-Murfreesboro-Franklin": {
+          "Cannon County": {},
+          "Cheahtam County": {},
+          "Davidson County": {},
+          "Dickson County": {},
+          "Macon County": {},
+          "Maury County": {},
+          "Robertson County": {},
+          "Rutherford County": {},
+          "Smith County": {},
+          "Sunner County": {},
+          "Trousdale County": {},
+          "Williamson County": {},
+          "Wilson County": {},
+        },
+      },
+    },
+  },
+};
