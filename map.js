9b6db52f672a6febc762ede4d04df6fa1301cec8
Ruslan Zyabbarov <48525052+zyabb@users.noreply.github.com>
2022-10-29 21:46:47 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,106 +1,115 @@
-
 setTimeout(() => {
-	const colors = {
-  activeStateColor: "#7569CF",
-  nonActiveStateColor: "#B9B8D8",
-};
+  const colors = {
+    activeStateColor: "#7569CF",
+    nonActiveStateColor: "#B9B8D8",
+  };
 
-const stateModals = {};
+  const stateModals = {};
 
-$($(".available-states")[0])
-  .find(".available-state-item div")
-  .each((idx, el) => {
-    const stateName = $(el).find(".state-name").text();
-    const stateDescription = $(el).find(".state-description p").html();
-    //TODO:
-    stateModals[stateName] = stateDescription;
+  $(".close-info-block").click(() => {
+    clearTableAndState();
+    $(".info-block").removeClass("show");
+    $(".tint").hide();
   });
 
-const stateEnabled = {
-  Alaska: false,
-  Florida: true,
-  Michigan: {
-    "MI-": false,
-    "SP-": false,
-  },
-  Vermont: false,
-  Maine: false,
-  Rhode_Island: false,
-  New_York: false,
-  Pennsylvania: false,
-  New_Jersey: false,
-  Delaware: false,
-  Maryland: false,
-  Virginia: true,
-  West_Virginia: false,
-  Ohio: true,
-  Indiana: true,
-  Illinois: false,
-  Connecticut: false,
-  Wisconsin: false,
-  North_Carolina: true,
-  District_of_Columbia: false,
-  Massachusetts: false,
-  Tennessee: true,
-  Arkansas: false,
-  Missouri: true,
-  Georgia: true,
-  South_Carolina: true,
-  Kentucky: false,
-  Alabama: true,
-  Louisiana: false,
-  Mississippi: false,
-  Iowa: false,
-  Minnesota: false,
-  Oklahoma: true,
-  Texas: true,
-  Nevada: false,
-  South_Dakota: false,
-  North_Dakota: false,
-  Wyoming: false,
-  Montana: false,
-  Colorado: true,
-  Idaho: false,
-  Utah: false,
-  Arizona: false,
-  Oregon: false,
-  Washington: false,
-  California: false,
-  Nebraska: false,
-  Kansas: false,
-  New_Mexico: false,
-  Hawaii: false,
-};
+  const clearTableAndState = () => {
+    $(".counties").remove();
+    $(".msa").remove();
+  };
 
-$description = $(".description");
+  const stateEnabled = {
+    Alaska: false,
+    Florida: true,
+    Michigan: {
+      "MI-": false,
+      "SP-": false,
+    },
+    Vermont: false,
+    Maine: false,
+    Rhode_Island: false,
+    New_York: false,
+    Pennsylvania: false,
+    New_Jersey: false,
+    Delaware: false,
+    Maryland: false,
+    Virginia: true,
+    West_Virginia: false,
+    Ohio: true,
+    Indiana: true,
+    Illinois: false,
+    Connecticut: false,
+    Wisconsin: false,
+    North_Carolina: true,
+    District_of_Columbia: false,
+    Massachusetts: false,
+    Tennessee: true,
+    Arkansas: false,
+    Missouri: true,
+    Georgia: true,
+    South_Carolina: true,
+    Kentucky: false,
+    Alabama: true,
+    Louisiana: false,
+    Mississippi: false,
+    Iowa: false,
+    Minnesota: false,
+    Oklahoma: true,
+    Texas: true,
+    Nevada: false,
+    South_Dakota: false,
+    North_Dakota: false,
+    Wyoming: false,
+    Montana: false,
+    Colorado: true,
+    Idaho: false,
+    Utah: false,
+    Arizona: false,
+    Oregon: false,
+    Washington: false,
+    California: false,
+    Nebraska: false,
+    Kansas: false,
+    New_Mexico: false,
+    Hawaii: false,
+  };
 
-Object.keys(stateEnabled)
-  .filter((element) => {
-    return stateEnabled[element] === true;
-  })
-  .forEach((element) => {
-    $(`#${element}`).addClass("enabled");
-    $(`#${element}`).attr("fill", colors.activeStateColor);
-    $(`#${element}`).hover(
-      //on hover over
-      function () {
-        $(this).attr("class", "enabled heyo");
-        $description.addClass("active");
-        $description.html(stateModals[element]);
-      },
-      //on hover out
-      function () {
-        $description.removeClass("active");
-        $description.html("");
-      }
-    );
-  });
+  // $description = $(".description");
+  const getMSAs = (state) => {
+    return Object.keys(states[state].MSA);
+  };
 
-$(document).on("mousemove", function (e) {
-  $description.css({
-    left: e.pageX,
-    top: e.pageY - 70,
-  });
-});
+  const getCountiesByMSA = (state, msa) => {
+    return Object.keys(states[state].MSA[msa]);
+  };
 
-}, 5000)
+  Object.keys(stateEnabled)
+    .filter((element) => {
+      return stateEnabled[element] === true;
+    })
+    .forEach((element) => {
+      $(`#${element}`).addClass("enabled");
+      $(`#${element}`).attr("fill", colors.activeStateColor);
+      $(`#${element}`).click(() => {
+        clearTableAndState();
+        console.log("clicked", element, states[element]);
+
+        const msas = getMSAs(element);
+        $(".state").text(element);
+        msas.forEach((el, idx) => {
+          $(`<div class=msa><div class=msa-text>${el}</div></div>`).appendTo(".table");
+          $("<div class=counties></div>").appendTo(".table");
+
+          const lastCounties = $(".counties").length - 1;
+          const counties = getCountiesByMSA(element, el);
+
+          const append = $(".counties")[lastCounties];
+
+          counties.forEach((el) => {
+            $(`<div class=county><div class=county-text>${el}</div></div>`).appendTo(append);
+          });
+        });
+        $(".tint").show();
+        $(".info-block").addClass("show");
+      });
+    });
+}, 5000);
